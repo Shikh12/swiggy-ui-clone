a23592e179a33ad5fb110d96a52dc5d6682659da
Shwetank Rai <raiishu1012@gmail.com>
2024-08-22 14:01:53 +0530

    Fix

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,21 @@
+import Category from "./components/Category";
+import Hrader from "./components/Hrader";
+import Section3 from "./components/Section3";
+import Section4 from"./components/Section4";
+import Section5 from"./components/Section5";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+   <>
+   <Hrader/>
+   <Category/>
+    <Section3/>
+    <Section4/>
+    <Section5/>
+    <Footer/>
+   </>
+  );
+}
+
+export default App;
